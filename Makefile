de216ad770056bfdc47eafb2adfcbdf72e5cf8ee
Dilawar Singh <dilawar.s.rajput@gmail.com>
2026-04-11 19:43:33 +0530

    Restructure repo: promote pandoc/ to root, remove old LaTeX resumes

--- Makefile
+++
@@ -1,4 +0,0 @@
-all : dilawar_singh.pdf  dilawar_singh_system.pdf
-
-%.pdf : %.tex
-	tectonic $<
--- commit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-pdflatex dilawar.tex
-git add *.sh 
-git add *.jpg
-git add *.tex
-git add dilawar.pdf
-git commit -m "$1"
-git push
